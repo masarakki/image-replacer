c081e61683ac24d8b064459fcd465cafd2dff576
masarakki <masaki182@gmail.com>
2020-01-05 16:53:01 +0900

    fix-condition

--- hello_world/app.rb
+++ hello_world/app.rb
@@ -12,51 +12,49 @@ def response(message = '')
   }
 end
 
+def enabled?(event)
+  event['type'] == 'message' && event['channel'] == 'CC2BFGC2Z' && event['user'] == 'UBYV3DW5S' && event['files']
+end
+
 def handle_event(event)
   slack = Faraday.new('https://slack.com/') do |conn|
     conn.response :json, content_type: /json/
   end
 
-  if event['type'] == 'message' && event['channel'] == 'CC2BFGC2Z'
-    # message_id = event['client_message_id']
-  end
-
-  if event['type'] == 'message' && event['files']
-    s3 = Aws::S3::Client.new
-    if event['user'] == 'UBYV3DW5S'
-      event['files'].select { |file| file['mimetype'].match?(/image/) && !file['is_external'] }.map do |file|
-        image = Faraday.get file['url_private_download'] do |req|
-          req.headers['Authorization'] = "Bearer #{ENV['USER_ACCESS_TOKEN']}"
-        end
-        thumb = Faraday.get file['thumb_360']  do |req|
-          req.headers['Authorization'] = "Bearer #{ENV['USER_ACCESS_TOKEN']}"
-        end
-        return p(:error_in_fetch_images, image, thumb) unless image.status == 200 && thumb.status == 200
-
-        key = "#{file['id']}.#{file['filetype']}"
-        s3.put_object(acl: 'public-read', body: image.body, content_type: file['mimetype'],
-                      bucket: ENV['BUCKET_NAME'], key: key)
-        object = Aws::S3::Object.new(bucket_name: ENV['BUCKET_NAME'], key: key)
-        thumbnail = Faraday::UploadIO.new(StringIO.new(thumb.body), file['mimetype'])
-        remote_file = slack.get('/api/files.remote.add',
-                                token: ENV['BOT_ACCESS_TOKEN'],
-                                external_id: file['id'], title: file['title'],
-                                external_url: object.public_url, preview_image: thumbnail)
-        return p(:error_in_remote_file_create, remote_file) unless remote_file.status == 200
-        p remote_file: remote_file
-        # slack.get('/api/files.remote.share', token: ENV['USER_ACCESS_TOKEN'], channels: event['channel'], file: remote_file.body['file']['id'])
-      end
+  s3 = Aws::S3::Client.new
+  event['files'].select { |file| file['mimetype'].match?(/image/) && !file['is_external'] }.map do |file|
+    image = Faraday.get file['url_private_download'] do |req|
+      req.headers['Authorization'] = "Bearer #{ENV['USER_ACCESS_TOKEN']}"
     end
+    thumb = Faraday.get file['thumb_360']  do |req|
+      req.headers['Authorization'] = "Bearer #{ENV['USER_ACCESS_TOKEN']}"
+    end
+    return p(:error_in_fetch_images, image, thumb) unless image.status == 200 && thumb.status == 200
+
+    key = "#{file['id']}.#{file['filetype']}"
+    s3.put_object(acl: 'public-read', body: image.body, content_type: file['mimetype'],
+                  bucket: ENV['BUCKET_NAME'], key: key)
+    object = Aws::S3::Object.new(bucket_name: ENV['BUCKET_NAME'], key: key)
+    thumbnail = Faraday::UploadIO.new(StringIO.new(thumb.body), file['mimetype'])
+    p ENV, file
+    remote_file = slack.get('/api/files.remote.add',
+                            token: ENV['BOT_ACCESS_TOKEN'],
+                            external_id: file['id'], title: file['title'],
+                            external_url: object.public_url, preview_image: thumbnail)
+    return p(:error_in_remote_file_create, remote_file) unless remote_file.status == 200
+    p remote_file: remote_file
+    # slack.get('/api/files.remote.share', token: ENV['USER_ACCESS_TOKEN'], channels: event['channel'], file: remote_file.body['file']['id'])
   end
 
   # noop!
 end
 
 def lambda_handler(event:, context:)
+  p event
   body = JSON.parse(event['body'])
   return response(challenge: body['challenge']) if body['type'] == 'url_verification'
 
   p body
-  handle_event(body['event']) if body['type'] == 'event_callback'
+  handle_event(body['event']) if body['type'] == 'event_callback' && enabled?(body['event'])
   response
 end
